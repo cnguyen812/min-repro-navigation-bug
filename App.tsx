047e9a231c5f9df602a7f822b5b4b5043f530f35
Chris Nguyen <cnguyen@bitpay.com>
2023-05-24 10:54:30 -0700

    Min repro for navigation bug

--- App.tsx
+++ App.tsx
@@ -1,118 +1,81 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer, NavigatorScreenParams} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import * as React from 'react';
+import {Text} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+type RootStackParamList = {
+  Tabs: NavigatorScreenParams<TabsStackParamsList>;
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+type TabsStackParamsList = {
+  Home: undefined;
+  Profile: undefined;
+};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+type HomeStackParamList = {
+  HomeRoot: undefined;
+};
+
+type ProfileStackParamList = {
+  ProfileRoot: undefined;
+};
+
+const Root = createStackNavigator<RootStackParamList>();
+const Tabs = createBottomTabNavigator<TabsStackParamsList>();
+const Home = createStackNavigator<HomeStackParamList>();
+const Profile = createStackNavigator<ProfileStackParamList>();
+
+const TabsStack = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <Tabs.Navigator detachInactiveScreens={true}>
+      <Tabs.Screen name="Home" component={HomeStack} />
+      <Tabs.Screen name="Profile" component={ProfileStack} />
+    </Tabs.Navigator>
   );
-}
+};
+
+const HomeStack = () => {
+  return (
+    <Home.Navigator>
+      <Home.Screen name="HomeRoot" component={HomeRoot} />
+    </Home.Navigator>
+  );
+};
+
+const HomeRoot = () => {
+  return (
+    <Text>
+      HomeRoot
+    </Text>
+  );
+};
+
+const ProfileStack = () => {
+  return (
+    <Profile.Navigator>
+      <Profile.Screen name="ProfileRoot" component={ProfileRoot} />
+    </Profile.Navigator>
+  );
+};
+
+const ProfileRoot = () => {
+  return (
+    <Text>
+      ProfileRoot
+    </Text>
+  );
+};
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Root.Navigator>
+        <Root.Screen name="Tabs" component={TabsStack} />
+      </Root.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
